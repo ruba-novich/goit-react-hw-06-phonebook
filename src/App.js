b85b2eaf8c3f4fa66c2807ddfa73c761ef6cfebd
ruba-novich <65170516+ruba-novich@users.noreply.github.com>
2021-09-03 00:00:19 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 
-import 'modern-normalize/modern-normalize.css';
-import './styles.css';
 import Container from './Components/Container';
 import Section from './Components/Section';
 import ContactForm from './Components/ContactForm';
 import SearchFilter from './Components/SearchFilter';
 import ContactList from './Components/ContactList';
 
+import 'modern-normalize/modern-normalize.css';
+import './styles.css';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = contact => {
     this.state.contacts.some(({ name }) => name === contact.name)
