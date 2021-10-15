fa441ea2e79e1ccb625a352024e86d1f13538243
ruba-novich <65170516+ruba-novich@users.noreply.github.com>
2021-10-15 23:47:48 +0300

    Updates

---
+++ precache-manifest.63bfc1879a70526b137ebec4b2645ac7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8720b8bf3c1c7cda95a025d817de5939",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "e59764cd20b3a378d510",
+    "url": "/goit-react-hw-06-phonebook/static/css/2.7ee642f7.chunk.css"
+  },
+  {
+    "revision": "12c329337bcd55baf68c",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.c9b9daf7.chunk.css"
+  },
+  {
+    "revision": "e59764cd20b3a378d510",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.67dd8f22.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.67dd8f22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c329337bcd55baf68c",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.35978dae.chunk.js"
+  },
+  {
+    "revision": "9d1a26fb56dbe9d98a08",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b0a8c527.js"
+  }
+]);
